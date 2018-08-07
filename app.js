088618eaf44642924c616704f0f8aa1232477b26
Unknown <ryantoltzman@gmail.com>
2018-08-07 01:32:21 -0700

    Initial garbage commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// top bar alert
+$(document).ready(function() {
+    $("#cart").click(function() {
+        $("#alert-show").show();
+   });
+});
+
+// modal 
+$('#signUpBtn').click(function(){
+    $('#signUpModal').modal('show')
+});
